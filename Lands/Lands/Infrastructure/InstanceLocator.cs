3506948c3ad77a08bc477f57c2c6e585437895a9
Sebastian <sebasm2003@hotmail.com>
2019-11-06 13:38:20 +0100

    Login page

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,14 @@
+﻿namespace Lands.Infrastructure
+{
+    using ViewModels;
+
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
